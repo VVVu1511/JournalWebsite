b9f7be4f6a4895ad580fdd1b2b161b8436aa5573
VVVu1511 <161138657+VVVu1511@users.noreply.github.com>
2025-02-28 06:53:10 +0700

    demo user page

--- journal_web/views.py
+++ journal_web/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Topic
+from .forms import TopicForm
 # Create your views here.
 
 def index(request):
@@ -13,9 +14,22 @@ def topics(request):
     return render(request,'journal_web/topics.html',context)   
 
 def topic(request,topic_id):
+    """Show 1 topic"""
     topic = Topic.objects.get(id=topic_id)
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic': topic,'entries': entries}
     
     return render(request,'journal_web/topic.html',context) 
+
+def new_topic(request):
+    """Add a new topic"""
+    if request.method != 'POST':
+        form = TopicForm()
+    else:
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('journal_web:topics')
+    context = {'form':form}
     
+    return render(request,'journal_web/new_topic.html',context)
