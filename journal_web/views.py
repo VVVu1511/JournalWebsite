3f572f16c83aa1699ab18d3fffb4510b6d2eac51
VVVu1511 <161138657+VVVu1511@users.noreply.github.com>
2025-03-02 21:27:14 +0700

    add new_entry

--- journal_web/views.py
+++ journal_web/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm,EntryForm
 # Create your views here.
 
 def index(request):
@@ -33,3 +33,20 @@ def new_topic(request):
     context = {'form':form}
     
     return render(request,'journal_web/new_topic.html',context)
+
+
+def new_entry(request,topic_id):
+    topic = Topic.objects.get(id=topic_id)
+    
+    if request.method != 'POST':
+        form = EntryForm()
+    else:
+        form = EntryForm(data=request.POST)
+        
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('journal_web:topic',topic_id=topic_id)
+    context = {'topic':topic,'form': form}
+    return render(request,'journal_web/new_entry.html',context)
